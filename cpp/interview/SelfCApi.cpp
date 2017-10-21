d27930aed839cb831860b97b63304fef144bb6d4
xjs <yiranshixin@126.com>
2017-10-22 01:01:41 +0800

    Update SelfCApi.cpp

--- cpp/interview/SelfCApi.cpp
+++ cpp/interview/SelfCApi.cpp
@@ -5,6 +5,7 @@ char *strcpy(char *strDest, constchar *strSrc)
        while ((*strDest++ = *strSrc++) != '\0') {};
        return p;
 }
+
 size_t strlen(char *p)
 {
        assert(!p);
@@ -14,4 +15,28 @@ size_t strlen(char *p)
               i++;
        }
        return i;
+}
+
+void memcpy(void *dst, void *src, size_t len)
+{
+       assert(!dst || !src || len < 0);
+       int pos = 0;
+       unsigned char *pdst = (unsigned char*)dst, *psrc = (unsigned char*)src;
+       if(src < dst)
+       {
+              pos = len - 1;
+              while(pos > 0)
+              {
+                     pdst[pos] = psrc[pos];
+                     pos--;
+              }
+       }
+       else
+       {
+              while (pos < len)
+              {
+                     pdst[pos] = psrc[pos];
+                     pos++;
+              }
+       }
 }
